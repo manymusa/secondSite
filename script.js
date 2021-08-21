de93aa6da824d6721556b8877b91702fabd1f429
adan <adanalin113@gmail.com>
2021-08-21 12:52:15 -0400

    updates to index.html style.css script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const resume = document.getElementById("resume");
 
 resume.addEventListener('click', function(){
-    alert('Resume isnt working right now, try again later!');
+    alert('Thank you for downloading!');
 });
